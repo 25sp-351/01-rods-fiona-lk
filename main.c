afbf4eea7aaa275626a45c284662ba09c5f2443d
Fiona Keaney <fionakeaney@csu.fullerton.edu>
2025-01-27 03:54:22 -0800

    added print and adjusted variable names to match style guide

--- main.c
+++ main.c
@@ -4,45 +4,67 @@
 #include <stdio.h>
 
 // recursion
-int cutRod(int rodLength, int price[], int cutList[], int *cutCount){
+int cutRod(int rod_length, int price[], int cutList[], int *cut_count) {
     // base case if no length yet
-    if (rodLength == 0) return 0;
+    if (rod_length == 0)
+        return 0;
 
-    int maxProfit = 0;
-    int bestCut = 0;
+    int max_profit = 0;
+    int best_cut   = 0;
 
     // iterations
-    for (int i =1; i <= rodLength; i++) {
-        int currentProfit = price[i - 1] + cutRod(l - 1, price, cutList, cutCount);
+    for (int i = 1; i <= rod_length; i++) {
+        int current_profit =
+            price[i - 1] + cutRod(rod_length - i, price, cutList, cut_count);
 
-        if (currentProfit > maxProfit){
-            maxProfit = currentProfit;
-            bestCut = i;
+        if (current_profit > max_profit) {
+            max_profit = current_profit;
+            best_cut   = i;
         }
     }
 
-    cutList[(*cutCount)++] = bestCut;
-    return maxProfit;
+    cutList[(*cut_count)++] = best_cut;
+    return max_profit;
 }
 
-void printResults(int cutList[], int cutCount, int price[], int rodLength) {
+void printResults(int cut_list[], int cut_count, int price[], int rod_length) {
+    int total_value = 0;
+    int length_used = 0;
 
-}
-
-int main(char *argv[]){
-    int rodLength = atoi(argv[1]);
-
-    int prices[50];
-    int count = 0;
-
-    while (scanf("%d", &prices[count]) != EOF) {
-        count++;
+    int counts[50] = {0};
+    for (int i = 0; i < cut_count; i++) {
+        counts[cut_list[i] - 1]++;
+        length_used += cut_list[i];
     }
 
-    int cutList[rodLength];
-    int cutCount = 0;
+    for (int i = 0; i < 50; i++) {
+        if (counts[i] > 0) {
+            int value = counts[i] * price[i];
+            total_value += value;
+            printf("%d @ %d = %d\n", counts[i], i + 1, value);
+        }
+    }
 
-    cutRod(rodLength, prices, cutList, &cutCount);
+    int remainder = rod_length - length_used;
+    printf("Remainder: %d\n", remainder);
+
+    printf("Value: %d\n", total_value);
+}
+
+int main(char *argv[]) {
+    int rod_length = atoi(argv[1]);
+
+    int prices[50];  // assuming max of 50
+    int count = 0;
+
+    while (scanf("%d", &prices[count]) != EOF)
+        count++;
+
+    int cut_list[rod_length];
+    int cut_count = 0;
+
+    cutRod(rod_length, prices, cut_list, &cut_count);
+    printResults(cut_list, prices, cut_list, &cut_count);
 
     return 0;
 }
